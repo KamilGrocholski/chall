95d5c765f4d5b220c62821730d8172dca22da467
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-03-28 23:53:17 +0200

    number

--- src/launch-countdown-timer/components/NumberBox.tsx
+++ src/launch-countdown-timer/components/NumberBox.tsx
@@ -1,17 +1,17 @@
 type NumberBoxProps = {
-    num: string | number
+    number: number
     unit: string
     flip: boolean
 }
 
-export const NumberBox: React.FC<NumberBoxProps> = ({ num, unit, flip }) => {
+export const NumberBox: React.FC<NumberBoxProps> = ({ number, unit, flip }) => {
     return (
         <div className='flex flex-col items-center overflow-hidden mt-4 lg:px-2 px-1'>
             <div className=' relative bg-transparent overflow-hidden flex flex-col items-center justify-center rounded-lg lg:w-32 lg:h-32 h-16 w-16 text-2xl md:text-2xl mt-4 '>
                 <div className='rounded-t-lg rounded-b-lg bg-[#2c2c44] w-full h-full'></div>
 
                 <div className='text-3xl absolute text-lct-counter-number z-0 font-bold font-redhat lg:text-7xl font-mono'>
-                    {num}
+                    {number}
                 </div>
 
                 <div className='z-5 rounded-b-lg rounded-t-lg bg-[#34364f] w-full h-full'></div>
@@ -21,7 +21,6 @@ export const NumberBox: React.FC<NumberBoxProps> = ({ num, unit, flip }) => {
                         flip ? 'animate-flip bg-lct-hills/50' : 'bg-transparent'
                     }`}
                 ></div>
-                {/* Two Small Dots */}
                 <div className='absolute -right-1.5 top-[58px] rounded-full w-[11px] h-[11px] bg-[#181721]'></div>
                 <div className='absolute -left-1.5 top-[58px] rounded-full w-[11px] h-[11px] bg-[#181721]'></div>
             </div>
