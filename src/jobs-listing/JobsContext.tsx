fe03a48f1b9e3cf664831ad23e4c15979e922485
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-04-11 23:28:17 +0200

    job-listing:filter

--- src/jobs-listing/JobsContext.tsx
+++ src/jobs-listing/JobsContext.tsx
@@ -1,224 +1,244 @@
-import data from './data.json'
+import data from "./data.json";
 import {
-    createContext,
-    useContext,
-    useEffect,
-    useReducer,
-    useState,
-} from 'react'
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 export type Job = {
-    id: number
-    company: string
-    logo: string
-    new: boolean
-    featured: boolean
-    position: string
-    role: string
-    level: string
-    postedAt: string
-    contract: string
-    location: string
-    languages: string[]
-    tools: string[]
-}
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+};
 
 export type JobsFilter<
-    T = Pick<Job, 'role' | 'level' | 'languages' | 'tools'>
+  T = Pick<Job, "role" | "level" | "languages" | "tools">
 > = {
-    [Key in keyof T]: T[Key] extends Array<any> ? T[Key] : T[Key] | null
-}
+  [Key in keyof T]: T[Key] extends Array<any> ? T[Key] : T[Key][];
+};
 
 type JobsContextType = {
-    jobs: Job[]
-    filteredJobs: Job[] | null
-    filter: JobsFilter
-    resetFilter(): void
-    filterActions: {
-        languages: {
-            add(language: string): void
-            remove(langueage: string): void
-        }
-        tools: {
-            add(tool: string): void
-            remove(tool: string): void
-        }
-        level: {
-            set(level: string): void
-            clear(): void
-        }
-        role: {
-            set(level: string): void
-            clear(): void
-        }
-    }
-}
+  jobs: Job[];
+  filteredJobs: Job[];
+  filter: JobsFilter;
+  resetFilter(): void;
+  filterActions: {
+    languages: {
+      add(language: string): void;
+      remove(langueage: string): void;
+    };
+    tools: {
+      add(tool: string): void;
+      remove(tool: string): void;
+    };
+    level: {
+      set(level: string): void;
+      remove(level: string): void;
+    };
+    role: {
+      set(role: string): void;
+      remove(role: string): void;
+    };
+  };
+};
 
-export const JobsContext = createContext<JobsContextType>({} as JobsContextType)
+export const JobsContext = createContext<JobsContextType>(
+  {} as JobsContextType
+);
 
-export const JobsProvider: React.FC<{ children: JSX.Element }> = ({
-    children,
+export const JobsProvider: React.FC<{ children: React.ReactElement }> = ({
+  children,
 }) => {
-    const [jobs, setJobs] = useState<Job[]>([])
-    const [filteredJobs, setFilteredJobs] = useState<Job[] | null>(null)
-    const [filter, setFilter] = useReducer<
-        (prev: JobsFilter, update: Partial<JobsFilter>) => JobsFilter,
-        JobsFilter
-    >(
-        (prev, update) => ({
-            ...prev,
-            ...update,
-        }),
-        createInitialJobsFilterState(),
-        createInitialJobsFilterState
-    )
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [filteredJobs, setFilteredJobs] = useState<Job[]>([]);
+  const [filter, setFilter] = useReducer<
+    (prev: JobsFilter, update: Partial<JobsFilter>) => JobsFilter,
+    JobsFilter
+  >(
+    (prev, update) => ({
+      ...prev,
+      ...update,
+    }),
+    createInitialJobsFilterState(),
+    createInitialJobsFilterState
+  );
 
-    useEffect(() => {
-        ;(async () => {
-            const result = await getJobs()
-            if (result) {
-                setJobs(result.data)
-            } else {
-                console.error('ok')
-            }
-        })()
-    }, [])
+  useEffect(() => {
+    (async () => {
+      const result = await getJobs();
+      if (result) {
+        setJobs(result.data);
+      } else {
+        console.error("ok");
+      }
+    })();
+  }, []);
 
-    useEffect(() => {
-        applyFilter()
-    }, [filter, jobs])
+  useEffect(() => {
+    applyFilter();
+  }, [filter, jobs]);
 
-    function applyFilter(): void {
-        const newFilteredJobs: Job[] = []
+  function applyFilter(): void {
+    const newFilteredJobs: Job[] = [];
 
-        jobs.forEach((job) => {
-            if (filter.level && filter.level === job.level) {
-                newFilteredJobs.push(job)
-                return
-            }
-            if (filter.role && filter.role === job.role) {
-                newFilteredJobs.push(job)
-                return
-            }
-            if (
-                filter.languages.length &&
-                job.languages.some(filter.languages.includes)
-            ) {
-                newFilteredJobs.push(job)
-                return
-            }
-            if (filter.tools.length && job.tools.some(filter.tools.includes)) {
-                newFilteredJobs.push(job)
-                return
-            }
-        })
+    jobs.forEach((job) => {
+      if (filter.role.length && filter.role.some((role) => role === job.role)) {
+        newFilteredJobs.push(job);
+        return;
+      }
+      if (
+        filter.level.length &&
+        filter.level.some((level) => level === job.level)
+      ) {
+        newFilteredJobs.push(job);
+        return;
+      }
+      if (
+        filter.languages.length &&
+        job.languages.some((language) =>
+          filter.languages.some((filterLanguage) => language === filterLanguage)
+        )
+      ) {
+        newFilteredJobs.push(job);
+        return;
+      }
+      if (
+        filter.tools.length &&
+        job.tools.some((tool) =>
+          filter.tools.some((filterTool) => tool === filterTool)
+        )
+      ) {
+        newFilteredJobs.push(job);
+        return;
+      }
+    });
 
-        setFilteredJobs(newFilteredJobs)
-    }
+    setFilteredJobs(newFilteredJobs);
+  }
 
-    const filterActions = {
-        languages: {
-            add(language: string): void {
-                const newLanguages = new Set(filter.languages)
-                newLanguages.add(language)
-                setFilter({
-                    languages: [...newLanguages],
-                })
-            },
-            remove(language: string): void {
-                const newLanguages = new Set(filter.languages)
-                newLanguages.delete(language)
-                setFilter({
-                    languages: [...newLanguages],
-                })
-            },
-        },
-        tools: {
-            add(tool: string): void {
-                const newTools = new Set(filter.tools)
-                newTools.add(tool)
-                setFilter({
-                    tools: [...newTools],
-                })
-            },
-            remove(tool: string): void {
-                const newTools = new Set(filter.tools)
-                newTools.delete(tool)
-                setFilter({
-                    tools: [...newTools],
-                })
-            },
-        },
-        role: {
-            set(role: string): void {
-                setFilter({
-                    role,
-                })
-            },
-            clear() {
-                setFilter({
-                    role: null,
-                })
-            },
-        },
-        level: {
-            set(level: string): void {
-                setFilter({
-                    level,
-                })
-            },
-            clear(): void {
-                setFilter({
-                    level: null,
-                })
-            },
-        },
-    }
+  const filterActions = {
+    languages: {
+      add(language: string): void {
+        const newLanguages = new Set(filter.languages);
+        newLanguages.add(language);
+        setFilter({
+          languages: [...newLanguages],
+        });
+      },
+      remove(language: string): void {
+        const newLanguages = new Set(filter.languages);
+        newLanguages.delete(language);
+        setFilter({
+          languages: [...newLanguages],
+        });
+      },
+    },
+    tools: {
+      add(tool: string): void {
+        const newTools = new Set(filter.tools);
+        newTools.add(tool);
+        setFilter({
+          tools: [...newTools],
+        });
+      },
+      remove(tool: string): void {
+        const newTools = new Set(filter.tools);
+        newTools.delete(tool);
+        setFilter({
+          tools: [...newTools],
+        });
+      },
+    },
+    role: {
+      set(role: string): void {
+        const newRole = new Set(filter.role);
+        newRole.add(role);
+        setFilter({
+          role: [...newRole],
+        });
+      },
+      remove(role: string) {
+        const newRole = new Set(filter.role);
+        newRole.delete(role);
+        setFilter({
+          role: [...newRole],
+        });
+      },
+    },
+    level: {
+      set(level: string): void {
+        const newLevel = new Set(filter.level);
+        newLevel.add(level);
+        setFilter({
+          level: [...newLevel],
+        });
+      },
+      remove(level: string) {
+        const newLevel = new Set(filter.level);
+        newLevel.delete(level);
+        setFilter({
+          level: [...newLevel],
+        });
+      },
+    },
+  };
 
-    function resetFilter() {
-        setFilter(createInitialJobsFilterState())
-    }
+  function resetFilter() {
+    setFilter(createInitialJobsFilterState());
+  }
 
-    return (
-        <JobsContext.Provider
-            value={{
-                resetFilter,
-                filter,
-                jobs,
-                filterActions,
-                filteredJobs,
-            }}
-        >
-            {children}
-        </JobsContext.Provider>
-    )
-}
+  return (
+    <JobsContext.Provider
+      value={{
+        resetFilter,
+        filter,
+        jobs,
+        filterActions,
+        filteredJobs,
+      }}
+    >
+      {children}
+    </JobsContext.Provider>
+  );
+};
 
 function createInitialJobsFilterState(): JobsFilter {
-    return {
-        languages: [],
-        level: null,
-        role: null,
-        tools: [],
-    }
+  return {
+    languages: [],
+    level: [],
+    role: [],
+    tools: [],
+  };
 }
 
 type Result<T> = {
-    data: T
-} | null
+  data: T;
+} | null;
 
 async function getJobs(): Promise<Result<Job[]>> {
-    try {
-        return {
-            data: data as unknown as Job[],
-        }
-    } catch (error) {
-        return null
-    }
+  try {
+    return {
+      data: data as unknown as Job[],
+    };
+  } catch (error) {
+    return null;
+  }
 }
 
 export default function useJobs() {
-    const jobsContext = useContext(JobsContext)
+  const jobsContext = useContext(JobsContext);
 
-    return jobsContext
+  return jobsContext;
 }
--- src/jobs-listing/index.tsx
+++ src/jobs-listing/index.tsx
@@ -1,112 +1,153 @@
-import clsx from 'clsx'
-import useJobs, { Job } from './JobsContext'
+import clsx from "clsx";
+import useJobs, { Job } from "./JobsContext";
 
 const JobsListingPage = () => {
-    const { jobs, filteredJobs } = useJobs()
+  const { jobs, filteredJobs } = useJobs();
 
-    return (
-        <div>
-            <header></header>
-            <main className='flex flex-col px-12'>
-                {filteredJobs && filteredJobs.length
-                    ? filteredJobs.map((job) => (
-                          <JobView {...job} key={job.id} />
-                      ))
-                    : jobs.map((job) => <JobView {...job} key={job.id} />)}
-            </main>
+  return (
+    <div>
+      <header></header>
+      <main className="flex flex-col px-12">
+        <JobsFilter />
+        <div className="flex flex-col">
+          {filteredJobs.length
+            ? filteredJobs.map((job) => <JobView {...job} key={job.id} />)
+            : jobs.map((job) => <JobView {...job} key={job.id} />)}
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
+
+const JobsFilter: React.FC = () => {
+  const { filter, filterActions } = useJobs();
+
+  return (
+    <div className="flex flex-row gap-5 border border-gray-500 p-5 rounded-lg">
+      {filter.level.map((level) => (
+        <JobFilterButton
+          key={level}
+          onClick={() => filterActions.level.remove(level)}
+        >
+          {level}
+        </JobFilterButton>
+      ))}
+      {filter.role.map((role) => (
+        <JobFilterButton
+          key={role}
+          onClick={() => filterActions.role.remove(role)}
+        >
+          {role}
+        </JobFilterButton>
+      ))}
+      {filter.languages.map((language) => (
+        <JobFilterButton
+          key={language}
+          onClick={() => filterActions.languages.remove(language)}
+        >
+          {language}
+        </JobFilterButton>
+      ))}
+      {filter.tools.map((tool) => (
+        <JobFilterButton
+          key={tool}
+          onClick={() => filterActions.tools.remove(tool)}
+        >
+          {tool}
+        </JobFilterButton>
+      ))}
+    </div>
+  );
+};
 
 const JobView: React.FC<Job> = (job) => {
-    const { filterActions } = useJobs()
+  const { filterActions } = useJobs();
 
-    return (
-        <div
-            className={clsx(
-                'border bg-white grid grid-cols-3 p-5 h-12 w-full',
-                job.featured && 'border-l-teal-500'
-            )}
-        >
-            <img src={job.logo} alt='' />
+  return (
+    <div
+      className={clsx(
+        "border bg-white grid grid-cols-3 p-5 w-full",
+        job.featured && "border-l-jl-primary"
+      )}
+    >
+      <img src={job.logo} alt="" />
 
-            <div className='flex flex-col'>
-                <div className='flex flex-row gap-3'>
-                    <span className='text-teal-500 font-semibold'>
-                        {job.company}
-                    </span>
-                    {job.new ? (
-                        <span className='rounded-2xl bg-teal-500 uppercase'>
-                            new!
-                        </span>
-                    ) : null}
-                    {job.featured ? (
-                        <span className='rounded-2xl bg-teal-900 uppercase'>
-                            featured
-                        </span>
-                    ) : null}
-                </div>
-            </div>
-
-            <div>
-                <span className='text-lg font-bold text-black'>
-                    {job.position}
-                </span>
-            </div>
-
-            <div className='flex flex-row gap-5 items-center'>
-                <span>{job.postedAt}</span>
-                <Dot />
-                <span>{job.contract}</span>
-                <Dot />
-                <span>{job.location}</span>
-            </div>
-
-            <div className='flex flex-row gap-3 items-center'>
-                <JobFilterButton
-                    onClick={() => filterActions.level.set(job.level)}
-                >
-                    {job.level}
-                </JobFilterButton>
-                <JobFilterButton
-                    onClick={() => filterActions.role.set(job.role)}
-                >
-                    {job.role}
-                </JobFilterButton>
-                {job.languages.map((language) => (
-                    <JobFilterButton
-                        onClick={() => filterActions.languages.add(language)}
-                    >
-                        {language}
-                    </JobFilterButton>
-                ))}
-                {job.tools.map((tool) => (
-                    <JobFilterButton
-                        onClick={() => filterActions.tools.add(tool)}
-                    >
-                        {tool}
-                    </JobFilterButton>
-                ))}
-            </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row gap-3">
+          <span className="text-jl-primary font-semibold">{job.company}</span>
+          {job.new ? (
+            <span className="rounded-2xl bg-jl-primary uppercase">new!</span>
+          ) : null}
+          {job.featured ? (
+            <span className="rounded-2xl bg-jl-secondary uppercase">
+              featured
+            </span>
+          ) : null}
         </div>
-    )
-}
+      </div>
+
+      <div>
+        <span className="text-lg font-bold text-black">{job.position}</span>
+      </div>
+
+      <div className="flex flex-row gap-5 items-center">
+        <span>{job.postedAt}</span>
+        <Dot />
+        <span>{job.contract}</span>
+        <Dot />
+        <span>{job.location}</span>
+      </div>
+
+      <div className="flex flex-row gap-3 items-center">
+        <JobFilterButton onClick={() => filterActions.level.set(job.level)}>
+          {job.level}
+        </JobFilterButton>
+        <JobFilterButton onClick={() => filterActions.role.set(job.role)}>
+          {job.role}
+        </JobFilterButton>
+        {job.languages.map((language) => (
+          <JobFilterButton
+            key={language}
+            onClick={() => filterActions.languages.add(language)}
+          >
+            {language}
+          </JobFilterButton>
+        ))}
+        {job.tools.map((tool) => (
+          <JobFilterButton
+            key={tool}
+            onClick={() => filterActions.tools.add(tool)}
+          >
+            {tool}
+          </JobFilterButton>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const JobFilterButton: React.FC<{
-    children: React.ReactNode
-    onClick: () => void
+  children: React.ReactNode;
+  onClick: () => void;
 }> = ({ children, onClick }) => {
-    return <button onClick={onClick}>{children}</button>
-}
+  return (
+    <button
+      className="bg-jl-accent hover:bg-jl-primary rounded hover:text-white text-jl-primary font-bold px-2 py-1"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
-const Dot: React.FC<{ size?: keyof typeof DOT_SIZE }> = ({ size = 'md' }) => {
-    return (
-        <div className={clsx('rounded-full bg-gray-500', DOT_SIZE[size])}></div>
-    )
-}
+const Dot: React.FC<{ size?: keyof typeof DOT_SIZE }> = ({ size = "md" }) => {
+  return (
+    <div className={clsx("rounded-full bg-gray-500", DOT_SIZE[size])}></div>
+  );
+};
 
 const DOT_SIZE = {
-    md: 'h-2 w-2',
-} as const
+  md: "h-2 w-2",
+} as const;
 
-export default JobsListingPage
+export default JobsListingPage;
