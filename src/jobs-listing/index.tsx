a9cc08927d4db6ef14009c33c9eeb5cb174d6c66
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-04-12 13:24:06 +0200

    jobs-listing:end

--- src/jobs-listing/index.tsx
+++ src/jobs-listing/index.tsx
@@ -1,153 +1,202 @@
-import clsx from "clsx";
-import useJobs, { Job } from "./JobsContext";
+import clsx from 'clsx'
+import useJobs, { Job } from './JobsContext'
+import { FaTimes } from 'react-icons/fa'
 
 const JobsListingPage = () => {
-  const { jobs, filteredJobs } = useJobs();
+    const { jobs, filteredJobs } = useJobs()
 
-  return (
-    <div>
-      <header></header>
-      <main className="flex flex-col px-12">
-        <JobsFilter />
-        <div className="flex flex-col">
-          {filteredJobs.length
-            ? filteredJobs.map((job) => <JobView {...job} key={job.id} />)
-            : jobs.map((job) => <JobView {...job} key={job.id} />)}
+    return (
+        <div className='relative w-full bg-jl-accent flex justify-center'>
+            <header className='w-full bg-jl-primary lg:bg-jlBgDesktop bg-jlBgMobile h-[155px]'></header>
+            <main className='absolute top-12 flex flex-col lg:gap-5 gap-24 py-16 px-12 container mx-auto'>
+                <JobsFilter />
+                <div className='flex flex-col lg:gap-6 gap-20'>
+                    {filteredJobs.length
+                        ? filteredJobs.map((job) => (
+                              <JobView {...job} key={job.id} />
+                          ))
+                        : jobs.map((job) => <JobView {...job} key={job.id} />)}
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
-};
+    )
+}
 
 const JobsFilter: React.FC = () => {
-  const { filter, filterActions } = useJobs();
+    const { filter, filterActions, resetFilter, isFilterEmpty } = useJobs()
 
-  return (
-    <div className="flex flex-row gap-5 border border-gray-500 p-5 rounded-lg">
-      {filter.level.map((level) => (
-        <JobFilterButton
-          key={level}
-          onClick={() => filterActions.level.remove(level)}
-        >
-          {level}
-        </JobFilterButton>
-      ))}
-      {filter.role.map((role) => (
-        <JobFilterButton
-          key={role}
-          onClick={() => filterActions.role.remove(role)}
-        >
-          {role}
-        </JobFilterButton>
-      ))}
-      {filter.languages.map((language) => (
-        <JobFilterButton
-          key={language}
-          onClick={() => filterActions.languages.remove(language)}
-        >
-          {language}
-        </JobFilterButton>
-      ))}
-      {filter.tools.map((tool) => (
-        <JobFilterButton
-          key={tool}
-          onClick={() => filterActions.tools.remove(tool)}
-        >
-          {tool}
-        </JobFilterButton>
-      ))}
-    </div>
-  );
-};
+    if (isFilterEmpty()) {
+        return null
+    }
+
+    return (
+        <div className='shadow-xl shadow-jl-primary/20 bg-white right-0 left-0 flex flex-row p-4 rounded-lg'>
+            <div className='flex grow flex-row flex-wrap gap-5'>
+                {filter.level.map((level) => (
+                    <JobFilterRemoveButton
+                        key={level}
+                        onClick={() => filterActions.level.remove(level)}
+                    >
+                        {level}
+                    </JobFilterRemoveButton>
+                ))}
+                {filter.role.map((role) => (
+                    <JobFilterRemoveButton
+                        key={role}
+                        onClick={() => filterActions.role.remove(role)}
+                    >
+                        {role}
+                    </JobFilterRemoveButton>
+                ))}
+                {filter.languages.map((language) => (
+                    <JobFilterRemoveButton
+                        key={language}
+                        onClick={() => filterActions.languages.remove(language)}
+                    >
+                        {language}
+                    </JobFilterRemoveButton>
+                ))}
+                {filter.tools.map((tool) => (
+                    <JobFilterRemoveButton
+                        key={tool}
+                        onClick={() => filterActions.tools.remove(tool)}
+                    >
+                        {tool}
+                    </JobFilterRemoveButton>
+                ))}
+            </div>
+
+            <button
+                onClick={resetFilter}
+                className='text-jl-primary hover:underline font-semibold'
+            >
+                Clear
+            </button>
+        </div>
+    )
+}
 
 const JobView: React.FC<Job> = (job) => {
-  const { filterActions } = useJobs();
+    const { filterActions } = useJobs()
 
-  return (
-    <div
-      className={clsx(
-        "border bg-white grid grid-cols-3 p-5 w-full",
-        job.featured && "border-l-jl-primary"
-      )}
-    >
-      <img src={job.logo} alt="" />
+    const logoSrc = new URL(job.logo, import.meta.url).href
 
-      <div className="flex flex-col">
-        <div className="flex flex-row gap-3">
-          <span className="text-jl-primary font-semibold">{job.company}</span>
-          {job.new ? (
-            <span className="rounded-2xl bg-jl-primary uppercase">new!</span>
-          ) : null}
-          {job.featured ? (
-            <span className="rounded-2xl bg-jl-secondary uppercase">
-              featured
-            </span>
-          ) : null}
+    return (
+        <div
+            className={clsx(
+                'relative border-l-4 bg-white grid lg:grid-cols-2 grid-cols-1 lg:divide-none divide-y p-5 w-full rounded-lg shadow-xl shadow-jl-primary/20',
+                job.featured ? 'border-l-jl-primary' : 'border-l-transparent'
+            )}
+        >
+            <div className='flex flex-row gap-5 lg:pt-0 pt-10'>
+                <div className='lg:static absolute -top-12 left-5'>
+                    <img src={logoSrc} alt='' className='h-24 w-24' />
+                </div>
+
+                <div className='flex flex-col text-white font-bold gap-2'>
+                    <div className='flex flex-row gap-3 flex-wrap'>
+                        <span className='text-jl-primary'>{job.company}</span>
+                        {job.new ? (
+                            <span className='rounded-2xl bg-jl-primary uppercase px-2 py-0.25'>
+                                new!
+                            </span>
+                        ) : null}
+                        {job.featured ? (
+                            <span className='rounded-2xl bg-jl-secondary uppercase px-2 py-0.25'>
+                                featured
+                            </span>
+                        ) : null}
+                    </div>
+
+                    <div>
+                        <span className='text-lg font-bold text-black'>
+                            {job.position}
+                        </span>
+                    </div>
+
+                    <div className='flex flex-row gap-5 items-center text-gray-500 font-medium flex-wrap'>
+                        <span>{job.postedAt}</span>
+                        <Dot />
+                        <span>{job.contract}</span>
+                        <Dot />
+                        <span>{job.location}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex flex-row gap-3 items-center flex-wrap lg:justify-end justify-sttart lg:py-0 py-5'>
+                <JobFilterAddButton
+                    onClick={() => filterActions.level.set(job.level)}
+                >
+                    {job.level}
+                </JobFilterAddButton>
+                <JobFilterAddButton
+                    onClick={() => filterActions.role.set(job.role)}
+                >
+                    {job.role}
+                </JobFilterAddButton>
+                {job.languages.map((language) => (
+                    <JobFilterAddButton
+                        key={language}
+                        onClick={() => filterActions.languages.add(language)}
+                    >
+                        {language}
+                    </JobFilterAddButton>
+                ))}
+                {job.tools.map((tool) => (
+                    <JobFilterAddButton
+                        key={tool}
+                        onClick={() => filterActions.tools.add(tool)}
+                    >
+                        {tool}
+                    </JobFilterAddButton>
+                ))}
+            </div>
         </div>
-      </div>
+    )
+}
 
-      <div>
-        <span className="text-lg font-bold text-black">{job.position}</span>
-      </div>
-
-      <div className="flex flex-row gap-5 items-center">
-        <span>{job.postedAt}</span>
-        <Dot />
-        <span>{job.contract}</span>
-        <Dot />
-        <span>{job.location}</span>
-      </div>
-
-      <div className="flex flex-row gap-3 items-center">
-        <JobFilterButton onClick={() => filterActions.level.set(job.level)}>
-          {job.level}
-        </JobFilterButton>
-        <JobFilterButton onClick={() => filterActions.role.set(job.role)}>
-          {job.role}
-        </JobFilterButton>
-        {job.languages.map((language) => (
-          <JobFilterButton
-            key={language}
-            onClick={() => filterActions.languages.add(language)}
-          >
-            {language}
-          </JobFilterButton>
-        ))}
-        {job.tools.map((tool) => (
-          <JobFilterButton
-            key={tool}
-            onClick={() => filterActions.tools.add(tool)}
-          >
-            {tool}
-          </JobFilterButton>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const JobFilterButton: React.FC<{
-  children: React.ReactNode;
-  onClick: () => void;
+const JobFilterRemoveButton: React.FC<{
+    children: React.ReactNode
+    onClick: () => void
 }> = ({ children, onClick }) => {
-  return (
-    <button
-      className="bg-jl-accent hover:bg-jl-primary rounded hover:text-white text-jl-primary font-bold px-2 py-1"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+    return (
+        <div className='flex itmes-center flex-row rounded'>
+            <span className='bg-jl-accent rounded text-jl-primary font-bold px-2 py-1 flex items-center'>
+                {children}
+            </span>
+            <button
+                onClick={onClick}
+                className='bg-jl-primary hover:bg-jl-secondary text-white flex items-center font-bold text-xl p-1 rounded-r'
+            >
+                <FaTimes />
+            </button>
+        </div>
+    )
+}
 
-const Dot: React.FC<{ size?: keyof typeof DOT_SIZE }> = ({ size = "md" }) => {
-  return (
-    <div className={clsx("rounded-full bg-gray-500", DOT_SIZE[size])}></div>
-  );
-};
+const JobFilterAddButton: React.FC<{
+    children: React.ReactNode
+    onClick: () => void
+}> = ({ children, onClick }) => {
+    return (
+        <button
+            className='bg-jl-accent hover:bg-jl-primary rounded hover:text-white text-jl-primary font-bold px-2 py-1'
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+const Dot: React.FC<{ size?: keyof typeof DOT_SIZE }> = ({ size = 'md' }) => {
+    return (
+        <div className={clsx('rounded-full bg-gray-500', DOT_SIZE[size])}></div>
+    )
+}
 
 const DOT_SIZE = {
-  md: "h-2 w-2",
-} as const;
+    md: 'h-2 w-2',
+} as const
 
-export default JobsListingPage;
+export default JobsListingPage
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,100 +1,107 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      display: ["Ubuntu", "monospace"],
-    },
-    extend: {
-      backgroundImage: {
-        sidebarDesktop:
-          "url('./src/multi-step-form/assets/images/bg-sidebar-desktop.svg')",
-        sidebarMobile:
-          "url('./src/multi-step-form/assets/images/bg-sidebar-mobile.svg')",
-        stars: "url(./src/launch-countdown-timer/images/bg-stars.svg)",
-        patternHills:
-          "url(./src/launch-countdown-timer/images/pattern-hills.svg)",
-        icdfCardBack:
-          "url(./src/interactive-card-details-form/images/bg-card-back.png)",
-        icdfCardFront:
-          "url(./src/interactive-card-details-form/images/bg-card-front.png)",
-        icdfCardLogo:
-          "url(./src/interactive-card-details-form/images/card-logo.svg)",
-        icdfDesktop:
-          "url(./src/interactive-card-details-form/images/bg-main-desktop.png)",
-        icdfMobile:
-          "url(./src/interactive-card-details-form/images/bg-main-mobile.png)",
-        icdfIconComplete:
-          "url(./src/interactive-card-details-form/images/icon-complete.svg)",
-        pcBgTop: "url(./src/pricing-component/images/bg-top.svg)",
-        pcBgBottom: "url(./src/pricing-component/images/bg-bottom.svg)",
-      },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            display: ['Ubuntu', 'monospace'],
+        },
+        extend: {
+            backgroundImage: {
+                sidebarDesktop:
+                    "url('./src/multi-step-form/assets/images/bg-sidebar-desktop.svg')",
+                sidebarMobile:
+                    "url('./src/multi-step-form/assets/images/bg-sidebar-mobile.svg')",
+                stars: 'url(./src/launch-countdown-timer/images/bg-stars.svg)',
+                patternHills:
+                    'url(./src/launch-countdown-timer/images/pattern-hills.svg)',
+                icdfCardBack:
+                    'url(./src/interactive-card-details-form/images/bg-card-back.png)',
+                icdfCardFront:
+                    'url(./src/interactive-card-details-form/images/bg-card-front.png)',
+                icdfCardLogo:
+                    'url(./src/interactive-card-details-form/images/card-logo.svg)',
+                icdfDesktop:
+                    'url(./src/interactive-card-details-form/images/bg-main-desktop.png)',
+                icdfMobile:
+                    'url(./src/interactive-card-details-form/images/bg-main-mobile.png)',
+                icdfIconComplete:
+                    'url(./src/interactive-card-details-form/images/icon-complete.svg)',
+                pcBgTop: 'url(./src/pricing-component/images/bg-top.svg)',
+                pcBgBottom: 'url(./src/pricing-component/images/bg-bottom.svg)',
+                jlBgDesktop:
+                    'url(./src/jobs-listing/images/bg-header-desktop.svg)',
+                jlBgMobile:
+                    'url(./src/jobs-listing/images/bg-header-mobile.svg)',
+            },
 
-      colors: {
-        primary: "#042958",
-        secondary: "#bee1ff",
-        muted: "#adaeb1",
-        third: "#483eff",
-        accent: "#7574e8",
-        accent2: "#eae8ff",
-        lct: {
-          sky: "#1e1e28",
-          hills: "#30243a",
-          counter: {
-            light: "#343650",
-            dark: "#2c2c44",
-            number: "#fa6085",
-          },
+            colors: {
+                primary: '#042958',
+                secondary: '#bee1ff',
+                muted: '#adaeb1',
+                third: '#483eff',
+                accent: '#7574e8',
+                accent2: '#eae8ff',
+                lct: {
+                    sky: '#1e1e28',
+                    hills: '#30243a',
+                    counter: {
+                        light: '#343650',
+                        dark: '#2c2c44',
+                        number: '#fa6085',
+                    },
+                },
+                pg: {
+                    light: '#24232b',
+                    dark: '#18171f',
+                    muted: '#4d4c54',
+                    primary: '#a4ffaf',
+                },
+                p: {
+                    text: '#2f304',
+                    gray: {
+                        light: '#eef1fa',
+                        dark: '#151a30',
+                        muted: '#abaaaf',
+                    },
+                    color: {
+                        red: '#f4726c',
+                        blue: '#6ef3f7',
+                        purple: '#d981f9',
+                    },
+                },
+                cp: {
+                    primary: '#8c92e7',
+                },
+                fylo: {
+                    bg: '#1c2230',
+                    primary: '#41afd0',
+                    active: '#5ab0ca',
+                },
+                jl: {
+                    primary: '#58a6a6',
+                    secondary: '#283938',
+                    accent: '#e3f9f7',
+                },
+            },
+            animation: {
+                flip: 'flip 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+            },
+            keyframes: {
+                flip: {
+                    from: {
+                        transform: 'rotateX(0deg)',
+                        transformOrigin: '50% bottom ',
+                    },
+                    to: {
+                        transform: 'rotateX(180deg)',
+                        transformOrigin: '50% bottom ',
+                    },
+                },
+            },
         },
-        pg: {
-          light: "#24232b",
-          dark: "#18171f",
-          muted: "#4d4c54",
-          primary: "#a4ffaf",
-        },
-        p: {
-          text: "#2f304",
-          gray: {
-            light: "#eef1fa",
-            dark: "#151a30",
-            muted: "#abaaaf",
-          },
-          color: {
-            red: "#f4726c",
-            blue: "#6ef3f7",
-            purple: "#d981f9",
-          },
-        },
-        cp: {
-          primary: "#8c92e7",
-        },
-        fylo: {
-          bg: "#1c2230",
-          primary: "#41afd0",
-          active: "#5ab0ca",
-        },
-        jl: {
-          primary: "#58a6a6",
-          secondary: "#283938",
-          accent: "#e3f9f7",
-        },
-      },
-      animation: {
-        flip: "flip 1s cubic-bezier(0, 0, 0.2, 1) infinite",
-      },
-      keyframes: {
-        flip: {
-          from: {
-            transform: "rotateX(0deg)",
-            transformOrigin: "50% bottom ",
-          },
-          to: {
-            transform: "rotateX(180deg)",
-            transformOrigin: "50% bottom ",
-          },
-        },
-      },
     },
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
