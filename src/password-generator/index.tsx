0bdd294dc33c4a709c53a4a5f94a768f15817181
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-04-17 10:20:56 +0200

    password generator: copy button, password strength

--- src/password-generator/index.tsx
+++ src/password-generator/index.tsx
@@ -1,15 +1,15 @@
-import { useReducer, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react';
 import {
     Config,
     PASSWORD_STRENGTH,
     PasswordStrengthType,
     generatePassword,
     getPasswordStrength,
-} from './utils'
-import useCopyToClipboard from './hooks/useCopyToClipboard'
-import clsx from 'clsx'
-import { FaRegCopy } from 'react-icons/fa'
-import { TiArrowRight } from 'react-icons/ti'
+} from './utils';
+import useCopyToClipboard from './hooks/useCopyToClipboard';
+import clsx from 'clsx';
+import { FaRegCopy } from 'react-icons/fa';
+import { TiArrowRight } from 'react-icons/ti';
 
 const generateDefaultConfig = (): Config => {
     return {
@@ -18,11 +18,11 @@ const generateDefaultConfig = (): Config => {
         uppercase: true,
         numbers: true,
         symbols: false,
-    }
-}
+    };
+};
 
 const PasswordGeneratorPage = () => {
-    const [showWarning, setShowWarning] = useState(false)
+    const [showWarning, setShowWarning] = useState(false);
 
     const [config, setConfig] = useReducer<
         (prev: Config, update: Partial<Config>) => Config,
@@ -36,9 +36,9 @@ const PasswordGeneratorPage = () => {
                     update.symbols ||
                     update.numbers)
             ) {
-                setShowWarning(false)
+                setShowWarning(false);
             }
-            return { ...prev, ...update }
+            return { ...prev, ...update };
         },
         {
             length: 5,
@@ -47,31 +47,31 @@ const PasswordGeneratorPage = () => {
             numbers: false,
             symbols: false,
         },
-        generateDefaultConfig
-    )
-    const [password, setPassword] = useState<undefined | string>(undefined)
+        generateDefaultConfig,
+    );
+    const [password, setPassword] = useState<undefined | string>(undefined);
 
     function handleGeneratePassowrd(e: React.FormEvent) {
-        e.preventDefault()
+        e.preventDefault();
         try {
-            const password = generatePassword(config)
-            setPassword(password)
+            const password = generatePassword(config);
+            setPassword(password);
         } catch (error) {
-            setShowWarning(true)
+            setShowWarning(true);
         }
     }
 
     return (
-        <div className='h-screen w-full bg-pg-dark text-white flex items-center'>
+        <div className="h-screen w-full bg-pg-dark text-white flex items-center">
             <form
                 onSubmit={handleGeneratePassowrd}
-                className='w-fit h-fit mx-auto flex flex-col space-y-5 min-w-[320px] md:min-w-[400px]'
+                className="w-fit h-fit mx-auto flex flex-col space-y-5 min-w-[320px] md:min-w-[400px]"
             >
-                <h1 className='text-pg-muted text-center text-2xl font-bold'>
+                <h1 className="text-pg-muted text-center text-2xl font-bold">
                     Password Generator
                 </h1>
-                <CopyText text={password} placeholder='P4$5W0rD!' />
-                <fieldset className='flex flex-col space-y-2 px-3 py-2 bg-pg-light'>
+                <CopyText text={password} placeholder="P4$5W0rD!" />
+                <fieldset className="flex flex-col space-y-2 px-3 py-2 bg-pg-light">
                     <Slider
                         label={'Character Length'}
                         min={1}
@@ -80,31 +80,31 @@ const PasswordGeneratorPage = () => {
                         onChange={(length) => setConfig({ length })}
                     />
                     {showWarning ? (
-                        <p className='text-red-500'>
+                        <p className="text-red-500">
                             'Check at least one of the following'
                         </p>
                     ) : null}
                     <Checkbox
-                        label='Include Uppercase Letters'
+                        label="Include Uppercase Letters"
                         isChecked={config.uppercase}
                         onChange={() =>
                             setConfig({ uppercase: !config.uppercase })
                         }
                     />
                     <Checkbox
-                        label='Include Lowercase Letters'
+                        label="Include Lowercase Letters"
                         isChecked={config.lowercase}
                         onChange={() =>
                             setConfig({ lowercase: !config.lowercase })
                         }
                     />
                     <Checkbox
-                        label='Include Numbers'
+                        label="Include Numbers"
                         isChecked={config.numbers}
                         onChange={() => setConfig({ numbers: !config.numbers })}
                     />
                     <Checkbox
-                        label='Include Symbols'
+                        label="Include Symbols"
                         isChecked={config.symbols}
                         onChange={() => setConfig({ symbols: !config.symbols })}
                     />
@@ -114,133 +114,144 @@ const PasswordGeneratorPage = () => {
                             ' w-full text-pg-light px-3 py-2 font-semibold border flex flex-row items-center justify-center text-lg',
                             !password?.length
                                 ? 'bg-pg-primary'
-                                : 'bg-pg-light border-pg-primary text-pg-primary'
+                                : 'bg-pg-light border-pg-primary text-pg-primary',
                         )}
-                        type='submit'
+                        type="submit"
                     >
                         <span>Generate</span>
-                        <TiArrowRight className='text-xl ml-1' />
+                        <TiArrowRight className="text-xl ml-1" />
                     </button>
                 </fieldset>
             </form>
         </div>
-    )
-}
+    );
+};
 
 const Slider: React.FC<{
-    min: number
-    max: number
-    disabled?: boolean
-    value: number
-    label: string
-    onChange: (value: number) => void
+    min: number;
+    max: number;
+    disabled?: boolean;
+    value: number;
+    label: string;
+    onChange: (value: number) => void;
 }> = ({ min, max, value, disabled, onChange, label }) => {
-    const [sliderValue, setSliderValue] = useState(value)
+    const [sliderValue, setSliderValue] = useState(value);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = Number(event.target.value)
-        setSliderValue(newValue)
-        onChange(newValue)
-    }
+        const newValue = Number(event.target.value);
+        setSliderValue(newValue);
+        onChange(newValue);
+    };
 
     return (
         <div>
-            <label className='flex flex-row justify-between items-center'>
+            <label className="flex flex-row justify-between items-center">
                 <span>{label}</span>
-                <span className='text-pg-primary font-semibold text-3xl'>
+                <span className="text-pg-primary font-semibold text-3xl">
                     {sliderValue}
                 </span>
             </label>
             <input
-                type='range'
+                type="range"
                 min={min}
                 max={max}
                 disabled={disabled}
                 aria-disabled={disabled}
                 value={sliderValue}
                 onChange={handleChange}
-                className='w-full slider'
+                className="w-full slider"
                 data-percent={(sliderValue / max) * 100}
             />
         </div>
-    )
-}
+    );
+};
 
 const Checkbox: React.FC<{
-    label: string
-    isChecked: boolean
-    onChange: () => void
+    label: string;
+    isChecked: boolean;
+    onChange: () => void;
 }> = ({ isChecked, onChange, label }) => {
     return (
-        <div className='flex items-center flex-row'>
+        <div className="flex items-center flex-row">
             <input
                 id={label}
-                type='checkbox'
+                type="checkbox"
                 checked={isChecked}
                 onChange={onChange}
-                className='bg-pg-light border border-white text-pg-primary focus:ring-pg-dark accent-pg-light'
+                className="bg-pg-light border border-white text-pg-primary focus:ring-pg-dark accent-pg-light"
             />
-            <label className='ml-2' htmlFor={label}>
+            <label className="ml-2" htmlFor={label}>
                 {label}
             </label>
         </div>
-    )
-}
+    );
+};
 
 const CopyText: React.FC<{ text?: string; placeholder?: string }> = ({
     text,
     placeholder,
 }) => {
-    const [, copyFn] = useCopyToClipboard()
+    const [showCopy, setShowCopy] = useState<boolean>(false);
+    const [, copyFn] = useCopyToClipboard();
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => setShowCopy(false), 1000);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [showCopy]);
 
     const handleCopyText = () => {
         if (!text) {
-            return
+            return;
         }
-        copyFn(text)
-    }
+        copyFn(text);
+        setShowCopy(true);
+    };
 
-    const isButtonDisabled = !text || !text.length
+    const isButtonDisabled = !text || !text.length;
 
     return (
-        <div className='flex flex-row bg-pg-light px-3 py-3 justify-between'>
+        <div className="flex flex-row bg-pg-light px-3 py-3 justify-between">
             <input
                 placeholder={placeholder}
                 value={text ?? ''}
                 disabled={true}
                 aria-disabled={true}
-                className='bg-pg-light placeholder:text-pg-muted text-white'
+                className="bg-pg-light placeholder:text-pg-muted text-white"
             />
             <button
-                type='button'
+                type="button"
                 disabled={isButtonDisabled}
                 aria-disabled={isButtonDisabled}
                 className={clsx(
                     'relative',
-                    isButtonDisabled && 'pointer-events-none'
+                    isButtonDisabled && 'pointer-events-none',
                 )}
                 onClick={handleCopyText}
             >
-                <div className='abolute bottom-3 left-4 bg-pg-light border-pg-primary px-3 py-2'>
-                    Copied!
-                </div>
-                <FaRegCopy className='text-pg-primary' />
+                {showCopy ? (
+                    <div className="absolute bottom-3 left-4 bg-pg-light border-pg-primary px-3 py-2">
+                        Copied!
+                    </div>
+                ) : null}
+                <FaRegCopy className="text-pg-primary" />
             </button>
         </div>
-    )
-}
+    );
+};
 
 const PasswordStrength: React.FC<{ strength: PasswordStrengthType }> = ({
     strength,
 }) => {
     return (
-        <div className='bg-pg-dark px-3 py-4 w-full flex flex-row justify-between items-center'>
-            <span className='uppercase text-pg-muted w-fit font-bold'>
+        <div className="bg-pg-dark px-3 py-4 w-full flex flex-row justify-between items-center">
+            <span className="uppercase text-pg-muted w-fit font-bold">
                 Strength
             </span>
-            <div className='flex flex-row items-center w-fit'>
-                <div className='font-semibold'>{strength}</div>
-                <div className='flex items-center flex-row'>
+            <div className="flex flex-row items-center w-fit">
+                <div className="font-semibold">{strength}</div>
+                <div className="flex items-center flex-row">
                     {Array.from({ length: 4 })
                         .fill(0)
                         .map((_, i) => (
@@ -248,14 +259,14 @@ const PasswordStrength: React.FC<{ strength: PasswordStrengthType }> = ({
                                 key={i}
                                 className={clsx(
                                     'w-3 h-7 border border-white ml-1',
-                                    PASSWORD_STRENGTH[strength]
+                                    PASSWORD_STRENGTH[strength],
                                 )}
                             ></div>
                         ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PasswordGeneratorPage
+export default PasswordGeneratorPage;
--- src/password-generator/utils.ts
+++ src/password-generator/utils.ts
@@ -1,15 +1,15 @@
 export type Config = {
-    length: number
-    uppercase: boolean
-    lowercase: boolean
-    numbers: boolean
-    symbols: boolean
-}
+    length: number;
+    uppercase: boolean;
+    lowercase: boolean;
+    numbers: boolean;
+    symbols: boolean;
+};
 
-const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' as const
-const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz' as const
-const NUMBERS = '0123456789' as const
-const SYMBOLS = `~!@#$%^&*()_+{}|:"<>?-=[]\\;\,./` as const
+const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' as const;
+const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz' as const;
+const NUMBERS = '0123456789' as const;
+const SYMBOLS = `~!@#$%^&*()_+{}|:"<>?-=[]\\;\,./` as const;
 
 export function generatePassword({
     length,
@@ -18,67 +18,90 @@ export function generatePassword({
     numbers,
     symbols,
 }: Config): string {
-    let pool = ''
+    let pool = '';
     if (uppercase) {
-        pool += UPPERCASE
+        pool += UPPERCASE;
     }
     if (lowercase) {
-        pool += LOWERCASE
+        pool += LOWERCASE;
     }
     if (numbers) {
-        pool += NUMBERS
+        pool += NUMBERS;
     }
     if (symbols) {
-        pool += SYMBOLS
+        pool += SYMBOLS;
     }
 
     if (!pool.length) {
-        throw new Error('pool must contain at least 1 character')
+        throw new Error('pool must contain at least 1 character');
     }
 
-    let password = ''
+    let password = '';
 
     for (let i = 0; i < length; ++i) {
-        const randomInt = getRandomIntInclusive(0, pool.length - 1)
-        password += pool[randomInt]
+        const randomInt = getRandomIntInclusive(0, pool.length - 1);
+        password += pool[randomInt];
     }
 
-    return password
+    return password;
 }
 
 function getRandomIntInclusive(min: number, max: number): number {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-export type PasswordStrengthType = keyof typeof PASSWORD_STRENGTH
+export type PasswordStrengthType = keyof typeof PASSWORD_STRENGTH;
 
 export const PASSWORD_STRENGTH = {
     'VERY WEAK': 'bg-red-800',
     WEAK: 'bg-red-500',
     MEDIUM: 'bg-orange-500',
     STRONG: 'bg-green-500',
-} as const
+    'VERY STRONG': 'bg-green-700',
+} as const;
 
 export function getPasswordStrength(config: Config): PasswordStrengthType {
-    let strength = 0
+    let strength = 0;
 
-    if (config.length < 5) return 'VERY WEAK'
-    if (config.length < 10) return 'WEAK'
+    if (config.numbers) {
+        strength++;
+    }
 
     if (config.symbols) {
-        strength += 3
-    }
-    if (config.numbers) {
-        strength += 1
-    }
-    if (config.uppercase) {
-        strength += 2
+        strength++;
     }
 
-    if (strength >= 6) return 'STRONG'
-    if (strength >= 5) return 'MEDIUM'
-    if (strength >= 4) return 'WEAK'
-    return 'VERY WEAK'
+    if (config.lowercase) {
+        strength++;
+    }
+
+    if (config.uppercase) {
+        strength++;
+    }
+
+    if (config.length >= 8) {
+        strength++;
+    }
+
+    if (config.length <= 3) {
+        strength = 0;
+    }
+
+    switch (strength) {
+        case 0:
+        case 1:
+            return 'VERY WEAK';
+        case 2:
+            return 'WEAK';
+        case 3:
+            return 'MEDIUM';
+        case 4:
+            return 'STRONG';
+        case 5:
+            return 'VERY STRONG';
+        default:
+            return 'VERY WEAK';
+    }
 }
